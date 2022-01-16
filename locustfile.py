e266eadd5f937639d384efbf485576c0998bc6af
Colin Shevlin <colin@classkick.com>
2022-01-16 14:58:42 -0600

    Checkpoint: getting metrics from locust.

--- locustfile.py
+++ locustfile.py
@@ -7,7 +7,7 @@ import logging
 
 HOST = 'localhost'
 PORT = 8080
-BUF_SIZE = 128
+BUF_SIZE = 64
 
 class WebSocketUser(User):
     abstract = True
@@ -16,31 +16,53 @@ class WebSocketUser(User):
         # This may need reformatting of the address
         gevent.spawn(self.receive_loop)
 
+    def context(self):
+        return {}
+
     def on_message(self, message):
         # Time the message and return the time
         current_timestamp = time.time()
         
         # Parse message body to get the timestamp
-        name = "socket send"
-        response_time = current_timestamp - time.time()
+        name = "roundtrip"
+        message = str(message, "utf-8").strip("\x00")
+        print(repr(message))
+        message = json.loads(message)
+        sent_at = message["sent_at"]
+        response_time = current_timestamp - sent_at
+        print(response_time)
 
         # send an event that we recieved a message
         self.environment.events.request.fire(
-            request_type="rx",
+            request_type="SOCKET",
             name=name,
             response_time=response_time,
             response_length=len(message),
             exception=None,
-            context=self.context(),
+            context=self.context()
         )
 
     def receive_loop(self):
         # receive data: https://docs.python.org/3/library/socket.html#socket.socket.recv
         while True:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                message = b"Hello work client"
+                body = {
+                    "sent_at": time.time()
+                }
+                message = json.dumps(body, indent=2).encode('utf-8')
                 s.connect((HOST, PORT))
                 s.sendall(message)
+                name = "roundtrip"
+
+                self.environment.events.request.fire(
+                    request_type="SOCKET",
+                    name=name,
+                    response_time=None,
+                    response_length=len(body),
+                    exception=None,
+                    context=self.context(),
+                )
+
                 message = s.recv(BUF_SIZE)
                 logging.debug(f"Recieved: {message} on socket {s}")
                 self.on_message(message)
