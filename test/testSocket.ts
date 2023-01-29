cbd0c1c86c75518b949a391875e5aa0f653a0008
colin-ck <colin@classkick.com>
2023-01-29 16:02:02 -0600

    Adds npm ignore.

---
+++ test/testSocket.ts
@@ -0,0 +1,22 @@
+import MutiplayerServerConnection from "..";
+
+
+let subscribe = (connection: MutiplayerServerConnection, topic: string) => {
+    connection.subscribe(topic, (message) => {
+        console.log(`Received message: ${message}`);
+    });
+}
+let publish = (connection: MutiplayerServerConnection, topic: string, message: string) => {
+    connection.set(topic, message);
+}
+
+let connection = new MutiplayerServerConnection("localhost:8000", "key")
+connection.register()
+    .then((success) => {
+        if (success) {
+            console.log(`Registration success: ${connection.userId}`)
+            subscribe(connection, "computers");
+        }
+    });
+
+
