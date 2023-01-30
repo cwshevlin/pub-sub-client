a394a4ef958ae98aeb4cdaa14752792c87679ac7
colin-ck <colin@classkick.com>
2023-01-29 20:20:43 -0600

    Modifies package to build repeatedly.

--- test/testSocket.ts
+++ test/testSocket.ts
@@ -1,22 +1,25 @@
 import MutiplayerServerConnection from "..";
 
+export default () => {
+    let subscribe = (connection: MutiplayerServerConnection, topic: string) => {
+        connection.subscribe(topic, (message) => {
+            console.log(`Received message: ${message}`);
+        });
+    }
+    let publish = (connection: MutiplayerServerConnection, topic: string, message: string) => {
+        connection.set(topic, message);
+    }
 
-let subscribe = (connection: MutiplayerServerConnection, topic: string) => {
-    connection.subscribe(topic, (message) => {
-        console.log(`Received message: ${message}`);
-    });
+    let connection = new MutiplayerServerConnection("localhost:8000", "key")
+    connection.register()
+        .then((success) => {
+            if (success) {
+                console.log(`Registration success: ${connection.userId}`)
+                subscribe(connection, "computers");
+            }
+        })
+        .catch((error) => {
+                console.log(`Registration error: userId: ${connection.userId}`)
+        })
+
 }
-let publish = (connection: MutiplayerServerConnection, topic: string, message: string) => {
-    connection.set(topic, message);
-}
-
-let connection = new MutiplayerServerConnection("localhost:8000", "key")
-connection.register()
-    .then((success) => {
-        if (success) {
-            console.log(`Registration success: ${connection.userId}`)
-            subscribe(connection, "computers");
-        }
-    });
-
-
